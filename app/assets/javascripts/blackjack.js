65615cf3a02775f017dd8181bfa766ffd9a27350
Gregory Gerla <gregorygerla1@gmail.com>
2014-06-14 14:58:53 -0700

    more module pattern

--- app/assets/javascripts/blackjack.js
+++ app/assets/javascripts/blackjack.js
@@ -3,27 +3,77 @@ var Deal = (function(){
   computerCards = [];
 
   function OneCard(){
-    playerCards.push((Math.floor(Math.random() * 13) + 1))
+    playerCards.push((Math.floor(Math.random() * 10) + 1))
+    computerCards.push((Math.floor(Math.random() * 10) + 1))
+  }
+  function hit(){
+    playerCards.push((Math.floor(Math.random() * 10) + 1))
   }
   function showCards(){
     return (playerCards)
+  }
+  function revealFirstCard(){
+    return (computerCards[0])
+  }
+  function bust(){
+
+    total = 0 
+    for (i=0;i<playerCards.length;i++){
+      total += playerCards[i] 
+    }
+    if (total > 21) {
+      return (true)
+    }else {
+      return (false)
+    }
   }
   return {
     deal: function(){
       OneCard()
     },
-    revealPlayerCards: showCards
+    revealPlayerCards: showCards,
+    revealFirstComputerCard: revealFirstCard,
+    hit : hit,
+    bust : bust
+
   }
+
 })()
+
+
+View = function(){
+
+}
+View.prototype = {
+  update : function(){
+    $('.players').text(Deal.revealPlayerCards());
+    $('.dealer').text(Deal.revealFirstComputerCard());
+  },
+  hit : function(){
+    $('.players').text(Deal.revealPlayerCards());
+  }
+
+}
+
+
 
 
 
 var program = (function(){
+  var view = new View
   function listeners(){
+    $(".hit").click(function(){
+      Deal.hit()
+      view.hit()
+      if (Deal.bust()){
+        console.log("bust")
+      }
+    }),
     $( ".start" ).click(function(){
       Deal.deal();
       Deal.deal();
-      $('.players').text(Deal.revealPlayerCards());
+      
+      view.update()
     })
   }
   return {
--- spec/javascripts/blackjackSpec.js
+++ spec/javascripts/blackjackSpec.js
@@ -1,7 +1,52 @@
 describe("Deal",function(){
-  it("works",function(){
     Deal.deal()
+  it("works",function(){
     expect(Deal.revealPlayerCards().length).toEqual(1);
+  }),
+  it("is less than 14",function(){
+    expect(Deal.revealPlayerCards()[0]).toBeLessThan(14);
+  })
 
+})
+describe("Deal",function(){
+  it("Deal to have been called",function(){
+    spyOn(Deal,'deal')
+    Deal.deal();
+    expect(Deal.deal).toHaveBeenCalled();
+  })
+})
+
+describe("playerCards",function(){
+  it("has playerCards",function(){
+    playerCards = [1,2]
+    Deal.deal()
+    Deal.deal()
+    expect(Deal.revealPlayerCards().length).toEqual(4)
+  })
+})
+
+describe("View",function(){
+  it("it will call View",function(){
+    spyOn(window,'View');
+    var view = new View()
+    expect(window.View).toHaveBeenCalled()
+  })
+})
+
+describe("Deal",function(){
+  it("should deal computer cards",function(){
+    Deal.deal();
+    expect(Deal.computerCards).tobeUndefined;
+
+  })
+})
+
+
+describe("View",function(){
+  it("it will call update",function(){
+    var view = new View()
+    spyOn(view,'update');
+    view.update();
+    expect(view.update).toHaveBeenCalled()
   })
 })
